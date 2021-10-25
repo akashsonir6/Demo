5f0236b8ac399d5ee6fada635451219732871e2f
Akash Soni <akashkumarsonir6@gmail.com>
2021-10-25 21:06:05 +0530

    ""

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import { Row,Col } from 'react-bootstrap';
 const Header = () => {
     return (
         <div>
             <div className="container">
                 <div className="header">
-                   <h1>Header</h1>
+                   <Row>
+    <Col xs={6} md={4}>
+     Company Logo
+    </Col>
+    <Col xs={6} md={4}>
+     Search
+    </Col>
+    <Col xs={6} md={4}>
+      Home
+    </Col>
+  </Row>
                 </div>
                 
         </div>
