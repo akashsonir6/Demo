a8e41a2043aededa1029442be6812f04fbbde6f9
Akash Soni <akashkumarsonir6@gmail.com>
2021-10-25 20:17:31 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+                <p>This is footer of Empty Project</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="header">
+                    <a href="">Home</a>
+                    <a href="">Contact</a>
+                    <a href="">About</a>
+                </div>
+                
+        </div>
+        </div>
+    )
+}
+
+export default Header
